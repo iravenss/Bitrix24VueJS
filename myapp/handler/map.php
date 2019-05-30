0c848cb5314379d11682acd745980ecd93a3172b
voronov.s <voronov.s@wiseadvice.ru>
2019-05-30 18:41:48 +0300

    Обаботчик

---
+++ myapp/handler/map.php
@@ -0,0 +1,25 @@
+<?php
+require_once __DIR__.'/../vendor/autoload.php';
+// TODO:Написать обработчик
+	?>
+
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+        <script type="text/javascript" src="//api.bitrix24.com/api/v1/"></script>
+    </head>
+<body>
+<div>
+    
+    
+    <pre><?print_r($_REQUEST);?></pre>
+    
+
+    <script>
+		
+    </script>
+
+</div>
+</body>
+</html>
